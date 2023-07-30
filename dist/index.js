4081ac3cad46ef37a978a01400c1cd9743b057e4
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-07-30 16:30:56 +0500

    first commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+//Basic Types
+let id = 5;
+let company = 'alidevs';
+let isPublished = true;
+let x = 'Hello';
+x = 5;
+console.log('x', x);
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+//Basic Types
+let id: number = 5;
+
+let company: string = 'alidevs';
+let isPublished: boolean = true;
+let x: any = 'Hello';           // could be any type.
+
+let names: string[] = ['ali', 'khan']; // arrays in TS, only store elements of same datatypes.
+
