60aa00b1b34518c8fa74b1cffdd92b230dfeaed4
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-07-31 14:00:16 +0500

    code updates

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,30 @@
 "use strict";
+//Basic Types
 let id = 5;
-
+let company = 'alidevs';
+let isPublished = true;
+let x = 'Hello'; // could be any type.
+var toCall = 'ali';
+let names = ['ali', 'khan']; // arrays in TS, only store elements of same datatypes.
+let ids = [1, 2, 3, 4];
+let arr = [1, true, 'Hello'];
+//Tuple:- 
+let person = [1, 'Ali', true];
+//Tuples Array:- 
+let employee;
+employee = [
+    [1, 'ali'],
+    [2, 'khan'],
+    [3, 'sam'],
+];
+//Unions:- 
+let pid = 2;
+//Enum 
+var Direction;
+(function (Direction) {
+    Direction["up"] = "up";
+    Direction["down"] = "down";
+    Direction["left"] = "left";
+    Direction["right"] = "right";
+})(Direction || (Direction = {}));
+console.log(Direction.up);
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,33 @@ let id: number = 5;
 
 let company: string = 'alidevs';
 let isPublished: boolean = true;
-let x: any = 'Hello';           // could be any type.
-
+let x: string = 'Hello';           // could be any type.
+var toCall: string = 'ali';
 let names: string[] = ['ali', 'khan']; // arrays in TS, only store elements of same datatypes.
+let ids: number[] = [1,2,3,4];
+let arr: any[] = [1, true, 'Hello'];
 
+
+//Tuple:- 
+let person: [number, string, boolean] = [1, 'Ali', true];
+
+//Tuples Array:- 
+let employee: [number, string][];
+employee = [
+    [1, 'ali'],
+    [2, 'khan'],
+    [3, 'sam'],
+];
+
+//Unions:- 
+let pid: string | number = 2;
+
+//Enum 
+enum Direction  {
+    up = 'up', 
+    down = 'down',
+    left = 'left',
+    right = 'right',
+}
+
+console.log(Direction.up);
